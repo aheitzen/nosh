88f6872f87b09858b96c5fe2481910c03ebfe35d
Alexa Heitzenrader <aheitzen1@gmail.com>
2018-09-29 17:32:40 -0700

    adding more js functionality

--- js/main.js
+++ js/main.js
@@ -1,12 +1,41 @@
 
 
-var rellax = new Rellax('.rellax', {
-        // center: true
-        callback: function(position) {
-            // callback every position change
-            console.log(position);
-        }
-      });
+// var rellax = new Rellax('.rellax', {
+//         // center: true
+//         callback: function(position) {
+//             // callback every position change
+//             console.log(position);
+//         }
+//       });
+
+
+
+// $( document ).ready(function() {
+// 	$("#h2__largeText").mouseenter(function() {
+//     	$( ".text.text__paragraph--container" ).css( "display", "block" );
+// 	}).mouseleave(function() {
+//     	$( ".text.text__paragraph--container" ).css( "display", "none" );
+// 	});
+// });
+
+
+
+
+
+$(document).ready(function() {
+	// hover over text, subheader floats up
+	$("#bagel-broker").mouseenter(function() {
+    	$( "#los-angeles" ).css( "display", "block" );
+	}).mouseleave(function() {
+    	$( "#los-angeles" ).css( "display", "none" );
+	});
+	// changing image on hover
+	$("#bagel-broker").mouseenter(function() {
+    	$(".img.img__top-ten").attr( "src", "images/R&D-5.jpg" );
+	}).mouseleave(function() {
+    	$( ".img.img__top-ten" ).attr( "src", "images/top-ten.jpeg" );
+	});
+});
 
 
 
