f75f118f14c130628e26b5033b85ea43321388b7
Alexa Heitzenrader <aheitzen1@gmail.com>
2018-09-30 18:33:12 -0700

    more js transitions

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ var scroll = new SmoothScroll('a[href*="#"]');
 
 $(document).ready(function() {
 	// hover over text, subheader floats up
-	$(".section.section__topTen").mouseenter(function() {
+	$("#broker-section").mouseenter(function() {
     	$( "#los-angeles" ).css({ 
     		"height": "auto",
     		"opacity": 1,
@@ -36,8 +36,14 @@ $(document).ready(function() {
     		"transition": "ease-out 1s"
     	});
 	});
+	// Text block function
+		$("#broker-section").mouseenter(function() {
+    	$( "#bagel-broker-text" ).css( "display", "block" );
+	}).mouseleave(function() {
+    	$( "#bagel-broker-text" ).css( "display", "none" );
+	});
 	//copy-block-transition
-	$(".section.section__topTen").mouseenter(function() {
+	$("#broker-section").mouseenter(function() {
     	$( "#bagel-broker-text" ).css({ 
     		"height": "auto",
     		"opacity": 1,
@@ -46,23 +52,19 @@ $(document).ready(function() {
 	}).mouseleave(function() {
     	$( "#bagel-broker-text" ).css({
     		"opacity": 0,
-    		"transition": "ease-out 1s"
+    		"transition": "ease-out .2s",
     	});
 	});
-	// Text block function
-		$(".section.section__topTen").mouseenter(function() {
-    	$( "#bagel-broker-text" ).css( "display", "block" );
-	}).mouseleave(function() {
-    	$( "#bagel-broker-text" ).css( "display", "none" );
-	});
 	// changing image on hover
-	$(".section.section__topTen").mouseenter(function() {
+	$("#broker-section").mouseenter(function() {
     	$(".img.img__top-ten").attr({ 
     		"src": "images/R&D-5.jpg",
     		"style":  "opacity: 1"
     	});
 	}).mouseleave(function() {
-    	$( ".img.img__top-ten" ).attr( "src", "images/top-ten.jpeg" );
+    	$( ".img.img__top-ten" ).attr({ 
+    		"src": "images/top-ten.jpeg",
+    	});
 	});
 	//CSS images transtitions
 	// $(".section.section__topTen").mouseenter(function() {
@@ -71,7 +73,7 @@ $(document).ready(function() {
  //    	$( ".img.img__top-ten" ).css( "transition", "ease-out 3s" );
 	// });
 	//Header change css/js
-	$(".section.section__topTen").mouseenter(function() {
+	$("#broker-section").mouseenter(function() {
     	$("#bagel-broker").css({
     		"color": "white", 
     		"-webkit-text-stroke-width": "1px",
@@ -81,10 +83,139 @@ $(document).ready(function() {
 	}).mouseleave(function() {
     	$( "#bagel-broker" ).css( "color", "blue" );
 	});
+
+
+
+	
+
+
+
+
+
+
+
+	// hover over text, subheader floats up
+	$("#bialy-section").mouseenter(function() {
+    	$( "#cleveland-ohio" ).css({ 
+    		"height": "auto",
+    		"opacity": 1,
+    		"transition": "ease-in 1s"
+    	});
+	}).mouseleave(function() {
+    	$( "#cleveland-ohio" ).css({
+    		"opacity": 0,
+    		"transition": "ease-out 1s"
+    	});
+	});
+	// Text block function
+		$("#bialy-section").mouseenter(function() {
+    	$( "#bialy-text" ).css( "display", "block" );
+	}).mouseleave(function() {
+    	$( "#bialy-text" ).css( "display", "none" );
+	});
+	//copy-block-transition
+	$("#bialy-section").mouseenter(function() {
+    	$( "#bialy-text" ).css({ 
+    		"height": "auto",
+    		"opacity": 1,
+    		"transition": "ease-in 1s"
+    	});
+	}).mouseleave(function() {
+    	$( "#bialy-text" ).css({
+    		"opacity": 0,
+    		"transition": "ease-out .2s",
+    	});
+	});
+	// changing image on hover
+	$("#bialy-section").mouseenter(function() {
+    	$(".img.img__top-ten").attr({ 
+    		"src": "images/test.jpeg",
+    		"style":  "opacity: 1"
+    	});
+	}).mouseleave(function() {
+    	$( ".img.img__top-ten" ).attr({ 
+    		"src": "images/top-ten.jpeg",
+    	});
+	});
+	//CSS images transtitions
+	// $(".section.section__topTen").mouseenter(function() {
+ //    	$(".img.img__top-ten").css( "transition", "ease-in 3s" );
+	// }).mouseleave(function() {
+ //    	$( ".img.img__top-ten" ).css( "transition", "ease-out 3s" );
+	// });
+	//Header change css/js
+	$("#bialy-section").mouseenter(function() {
+    	$("#bb-header").css({
+    		"color": "white", 
+    		"-webkit-text-stroke-width": "1px",
+    		"-webkit-text-stroke-color": "blue",
+    		"cursor": "not-allowed"
+    	});
+	}).mouseleave(function() {
+    	$( "#bb-header" ).css( "color", "blue" );
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// //NEXT SECTION: Bialy Section
+	// // header change
+	// $("#bialy-section").mouseenter(function() {
+ //    	$( "#cleveland-ohio" ).css({ 
+ //    		"height": "auto",
+ //    		"opacity": 1,
+ //    		"transition": "ease-in 1s"
+ //    	});
+	// }).mouseleave(function() {
+ //    	$( "#cleveland-ohio" ).css({
+ //    		"opacity": 0,
+ //    		"transition": "ease-out 1s"
+ //    	});
+	// });
+	// //image change 
+	// $("#bialy-section").mouseenter(function() {
+ //    	$(".img.img__top-ten").attr({ 
+ //    		"src": "images/test.jpeg",
+ //    		"style":  "opacity: 1"
+ //    	});
+	// }).mouseleave(function() {
+ //    	$( ".img.img__top-ten" ).attr( "src", "images/top-ten.jpeg" );
+	// });
+	// // Text block function
+	// $("#bialy-section").mouseenter(function() {
+ //    	$( "#bialy-text" ).css( "display", "block" );
+	// }).mouseleave(function() {
+ //    	$( "#bialy-text" ).css( "display", "none" );
+	// });
+	// //copy-block-transition
+	// $("#bialy-section").mouseenter(function() {
+ //    	$( "#bialy-text" ).css({ 
+ //    		"height": "auto",
+ //    		"opacity": 1,
+ //    		"transition": "ease-in 1s"
+ //    	});
+	// }).mouseleave(function() {
+ //    	$( "#bialy-text" ).css({
+ //    		"opacity": 0,
+ //    		"transition": "ease-out 1s",
+ //    		// "height": 0
+ //    	});
+	// });
+
+
 });
 
 
-// "transition": "ease-out .4s"
+
 
 
 
